ead31fc285155798efc82a593e6fdad43a01f811
Jenny Citherlet <jennycith@gmail.com>
2020-07-28 20:46:30 +0000

    Refraction of spinner1.js

---
+++ spinner2.js
@@ -0,0 +1,45 @@
+const sentence = (['\r|  ', '\r/  ', '\r-  ', '\r\\   ', '\r|  ', '\r/    ', '\r-  ', '\r\\  ', '\r|  ']);
+
+for (const char in sentence) {
+  setTimeout(() => {
+    process.stdout.write(sentence[char]);
+  }, 200 + char * 200);
+};
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   ');
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1300);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   ');
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 1700);
